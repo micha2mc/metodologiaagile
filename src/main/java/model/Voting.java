9cc23007c82311116f9597717c12159fadfdde45
micha <micha2mc@gmail.com>
2024-12-11 22:33:16 +0100

    [SC11] Modelo y DAO de equipo y Votacion

---
+++ src/main/java/model/Voting.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Voting {
+    private int nid;
+    private String titulo;
+    private String descripcion;
+    private LocalDate fechaLimite;
+}
